3cc836236ffa6fd52c307b8f9021fd682982b2ba
Beinan Wang <beinanw@twitter.com>
2020-09-18 23:44:59 -0700

    Implement main menu with a drawer

--- src/layouts/Dashboard.tsx
+++ src/layouts/Dashboard.tsx
@@ -1,21 +1,140 @@
 import * as React from 'react';
+import clsx from 'clsx';
 import AppBar from '@material-ui/core/AppBar';
+import Drawer from '@material-ui/core/Drawer';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
 import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import DashboardIcon from '@material-ui/icons/Dashboard';
+import LayersIcon from '@material-ui/icons/Layers';
+import { makeStyles } from '@material-ui/core/styles';
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+    },
+    appBar: {
+        zIndex: theme.zIndex.drawer + 1,
+        transition: theme.transitions.create(['width', 'margin'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+    },
+    appBarShift: {
+        marginLeft: drawerWidth,
+        width: `calc(100% - ${drawerWidth}px)`,
+        transition: theme.transitions.create(['width', 'margin'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+    },
+    menuButton: {
+        marginRight: 36,
+    },
+    hide: {
+        display: 'none',
+    },
+    drawer: {
+        width: drawerWidth,
+        flexShrink: 0,
+        whiteSpace: 'nowrap',
+    },
+    drawerOpen: {
+        width: drawerWidth,
+        transition: theme.transitions.create('width', {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+    },
+    drawerClose: {
+        transition: theme.transitions.create('width', {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+        overflowX: 'hidden',
+        width: theme.spacing(7) + 1,
+        [theme.breakpoints.up('sm')]: {
+            width: theme.spacing(9) + 1,
+        },
+    },
+    toolbar: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+        padding: theme.spacing(0, 1),
+        // necessary for content to be below app bar
+        ...theme.mixins.toolbar,
+    }
+}));
+
 
 function Dashboard() {
+    const classes = useStyles();
+    const [drawerState, setDrawerState] = React.useState(true);
+    const openDrawer = () => {
+        setDrawerState(true);
+    };
+    const closeDrawer = () => {
+        setDrawerState(false);
+    };
     return (
-        <div>
-            <AppBar>
+        <div className={classes.root}>
+            <AppBar
+                position="fixed"
+                className={clsx(classes.appBar, {
+                [classes.appBarShift]: drawerState,
+            })}>
                 <Toolbar>
-                    <IconButton>
+                    <IconButton
+                        onClick={openDrawer}
+                        className={clsx(classes.menuButton, {
+                        [classes.hide]: drawerState,
+                    })}>
                         <MenuIcon></MenuIcon>
                     </IconButton>
-                    <Typography>Presto Dashboard</Typography>
+                    <Typography variant="h6">Presto Dashboard</Typography>
                 </Toolbar>
             </AppBar>
+            <Drawer open={drawerState}
+                    variant="permanent"
+                    className={clsx(classes.drawer, {
+                        [classes.drawerOpen]: drawerState,
+                        [classes.drawerClose]: !drawerState,
+                    })}
+                    classes={{
+                        paper: clsx({
+                            [classes.drawerOpen]: drawerState,
+                            [classes.drawerClose]: !drawerState,
+                        }),
+                    }}>
+                <div className={classes.toolbar}>
+                    <IconButton onClick={closeDrawer}>
+                        <ChevronLeftIcon />
+                    </IconButton>
+                </div>
+                <List>
+                    <ListItem button>
+                        <ListItemIcon>
+                            <DashboardIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="Dashboard" />
+                    </ListItem>
+                    <ListItem button>
+                        <ListItemIcon>
+                            <LayersIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="Queries" />
+                    </ListItem>
+                </List>
+            </Drawer>
         </div>
     );
 }
